b7e966b3ccbf82321e1e2fe719bb6b9ea2283312
akshayda04 <akshay.agrawal@pykih.com>
2016-04-10 15:02:08 +0530

    added styled map to the page

---
+++ src/js/map.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiYWtzaGF5MDQiLCJhIjoiMG5sWGx0WSJ9.YbvjfBs2-EvdTSftGgzfbA';
+var map = L.mapbox.map('map');
+L.mapbox.styleLayer('mapbox://styles/akshay04/cimub8uhg008x9wnj11f2e5yr').addTo(map);
